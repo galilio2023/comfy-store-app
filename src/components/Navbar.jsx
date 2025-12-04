9dc20beb1456cae502f502292be663016e952525
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-04 09:10:00 +0200

    add darktheme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,26 @@ import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
+const loadTheme = () => {
+  return localStorage.getItem("theme") || themes.winter;
+};
 const Navbar = () => {
-  const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(loadTheme());
   const handleTheme = () => {
-    setTheme(!theme);
+    const { winter, dracula } = themes;
+    const newTheme = theme === winter ? dracula : winter;
+    setTheme(newTheme);
   };
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element">
