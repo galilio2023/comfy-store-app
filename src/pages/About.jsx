d52f7acdcbc75a44c7fddd4b48e583dd5344ee93
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-05 02:15:23 +0200

    finish about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const About = () => {
-  return <h1 className="text-3xl">About page</h1>;
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+        provident hic, sint officia iure non asperiores facere dolor blanditiis
+        assumenda vitae dolorum. Odit cumque rem quaerat, esse at,
+        exercitationem autem adipisci quam tempore in culpa possimus ab eligendi
+        mollitia accusamus?
+      </p>
+    </>
+  );
 };
 
 export default About;
