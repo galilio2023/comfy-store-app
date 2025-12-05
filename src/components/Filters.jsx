fc98b44bc1caa50ec33c329752114b4d6ac5e94a
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-06 00:55:28 +0200

    create components for filters

---
+++ src/components/Filters.jsx
@@ -0,0 +1,55 @@
+import { Form, useLoaderData, Link } from "react-router-dom";
+import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
+import FormRange from "./FormRange";
+import FormCheckbox from "./FormCheckbox";
+
+const Filters = () => {
+  const { meta } = useLoaderData();
+
+  return (
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* search */}
+      <FormInput
+        type="search"
+        label="search product"
+        name="search"
+        size="input-sm"
+      />
+      {/* categories */}
+      <FormSelect
+        label="select category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+      {/* companies */}
+      <FormSelect
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      {/* order */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={["a-z", "z-a", "high", "low"]}
+        size="select-sm"
+      />
+      {/* price */}
+      <FormRange name="price" label="select price" size="range-sm" />
+      {/* shipping */}
+      <FormCheckbox name="shipping" label="free shipping" size="checkbox-sm" />
+      {/* buttons */}
+      <button type="submit" className="btn btn-primary btn-sm">
+        search
+      </button>
+      <Link to="/products" className="btn btn-accent btn-sm">
+        reset
+      </Link>
+    </Form>
+  );
+};
+
+export default Filters;
