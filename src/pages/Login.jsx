8fbd06175d0721d4462e9b92d783f17ef234374f
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-06 09:41:31 +0200

    finish login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,28 @@
 import { FormInput, SubmitBtn } from "../components";
+import { Form, Link, redirect, useNavigate } from "react-router-dom";
+import { customFetch } from "../utils";
+import { toast } from "react-toastify";
+import { loginUser } from "../features/user/userSlice";
+import { useDispatch } from "react-redux";
 
-import { Form, Link } from "react-router-dom";
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    try {
+      const response = await customFetch.post("/auth/local", data);
+      store.dispatch(loginUser(response.data));
+      toast.success("logged in successfully");
+      return redirect("/");
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "please check your credentials";
+      toast.error(errorMessage);
+      return null;
+    }
+  };
 
 const Login = () => {
   return (
