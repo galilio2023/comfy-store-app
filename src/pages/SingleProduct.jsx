6cc4854788a98038e06701153a1172ea5f8ee2c4
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-06 04:29:36 +0200

    finish cart actions

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -3,6 +3,8 @@ import { formatPrice, customFetch, generateAmountOptions } from "../utils";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import Products from "./Products";
+import { useDispatch } from "react-redux";
+import { addItem } from "../features/cart/cartSlice";
 
 export const loader = async ({ params }) => {
   const response = await customFetch(`/products/${params.id}`);
@@ -18,6 +20,20 @@ const SingleProduct = () => {
   const [amount, setAmount] = useState(1);
   const handleAmount = (e) => {
     setAmount(parseInt(e.target.value));
+  };
+  const cartProduct = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image,
+    title,
+    price,
+    company,
+    productColor,
+    amount,
+  };
+  const dispatch = useDispatch();
+  const addToCart = () => {
+    dispatch(addItem({ product: cartProduct }));
   };
   return (
     <section>
@@ -89,10 +105,7 @@ const SingleProduct = () => {
           </div>
           {/* Card btn */}
           <div className="mt-10">
-            <button
-              className="btn btn-secondary btn-md"
-              onClick={() => console.log("add to bag")}
-            >
+            <button className="btn btn-secondary btn-md" onClick={addToCart}>
               Add to bag
             </button>
           </div>
