487b4b94ef69e2275f67132a34c60c93c3e0a32e
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-06 08:32:52 +0200

    finish register

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import { loader as landingLoader } from "./pages/Landing";
 import { loader as singleProductLoader } from "./pages/SingleProduct";
 import { loader as productLoader } from "./pages/Products";
 //actions
+import { action as registerAction } from "./pages/Register";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -70,6 +71,7 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
     errorElement: <Error />,
+    action: registerAction,
   },
 ]);
 
