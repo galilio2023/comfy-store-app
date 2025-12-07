83935b00e339e549f0a08ff78c355bc348fbd79e
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-08 01:11:16 +0200

    add query to singleProduct

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -11,7 +11,7 @@ const featuredProductsQuery = {
 
 export const loader = (queryClient) => async () => {
   const response = await queryClient.ensureQueryData(featuredProductsQuery);
-  console.log(response);
+
   const products = response.data.data;
   return { products };
 };
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -6,10 +6,19 @@ import Products from "./Products";
 import { useDispatch } from "react-redux";
 import { addItem } from "../features/cart/cartSlice";
 
+const singleProductQuery = (id) => {
+  return {
+    queryKey: ["singleProduct", id],
+    queryFn: () => customFetch(`/products/${id}`),
+  };
+};
+
 export const loader =
   (queryClient) =>
   async ({ params }) => {
-    const response = await customFetch(`/products/${params.id}`);
+    const response = await queryClient.ensureQueryData(
+      singleProductQuery(params.id)
+    );
     return { product: response.data.data };
   };
 
