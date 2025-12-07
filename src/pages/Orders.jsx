6dbb9958216f8699fb7f05ca30c5f4a26ae3923b
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-12-08 01:37:55 +0200

    add query to orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -7,8 +7,24 @@ import {
   SectionTitle,
 } from "../components";
 
+const ordersQuery = (params, user) => {
+  return {
+    queryKey: [
+      "orders",
+      user.username,
+      params.page ? parseInt(params.page) : 1,
+    ],
+    queryFn:()=>customFetch.get("/orders", {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+  };
+};
+
 export const loader =
-  (store) =>
+  (store,queryClient) =>
   async ({ request }) => {
     const user = store.getState().userState.user;
     if (!user) {
@@ -20,12 +36,7 @@ export const loader =
       ...new URL(request.url).searchParams.entries(),
     ]);
     try {
-      const response = await customFetch.get("/orders", {
-        params,
-        headers: {
-          Authorization: `Bearer ${user.token}`,
-        },
-      });
+      const response = await queryClient.ensureQueryData(ordersQuery(params,user));
 
       return { orders: response.data.data, meta: response.data.meta };
     } catch (error) {
@@ -34,7 +45,7 @@ export const loader =
         error?.response?.data?.error?.message ||
         "there was an error placing your order";
       toast.error(errorMessage);
-      if (error.response.status === 401 || error?.response?.status === 403)
+      if (error?.response?.status === 401 || error?.response?.status === 403)
         return redirect("/login");
       return null;
     }
